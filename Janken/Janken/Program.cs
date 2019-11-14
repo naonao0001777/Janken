7d0d5e622a67e8ce458aa10e05e34d2b562b2af3
直弘 安孫子 <abi1060special@gmail.com>
2019-11-14 23:17:41 +0900

    プロジェクトを作成しただけ

---
+++ Janken/Janken/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jankenAA = 
+                { ""
+                ,""
+                ,""
+            };
+        }
+    }
+}
