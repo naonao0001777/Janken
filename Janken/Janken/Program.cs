602739d03df375deaed497d5b92b388d75fcee81
直弘 安孫子 <abi1060special@gmail.com>
2019-11-15 00:25:04 +0900

    じゃんけん完成

--- Janken/Janken/Program.cs
+++ Janken/Janken/Program.cs
@@ -8,13 +8,85 @@ namespace Janken
 {
     class Program
     {
+        public static string[] jankenAA = { "ぐー", "ちょき", "ぱー" };
+
         static void Main(string[] args)
         {
-            string[] jankenAA = 
-                { ""
-                ,""
-                ,""
-            };
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("最初はグー、じゃんけん…");
+                int pjank = Player();
+                int cpjank= Cpu();
+                Judge(pjank,cpjank);
+            }
+        }
+        public static int Player()
+        {
+            string janken = null;
+            bool loopFlg = true;
+            while (loopFlg)
+            {
+                janken = Console.ReadLine();
+
+                if (!(janken.Equals("0") || janken.Equals("1") || janken.Equals("2")))
+                {
+                    Console.WriteLine("0か1か2で入れてください。");
+                    loopFlg = true;
+                }
+                else
+                {
+                    loopFlg = false;
+                }
+            }
+            int jank = int.Parse(janken);
+            Display(0,jank);
+            return jank;
+        }
+
+        public static int Cpu()
+        {
+            int randomInt = Janken();
+            Display(1,randomInt);
+            return randomInt;
+        }
+
+        public static void Display(int isPlayer,int jank)
+        {
+            if (isPlayer == 0)
+            {
+                Console.Write("あなたは、");
+            }
+            else
+            {
+                Console.Write("コンピューターは、");
+            }
+            Console.WriteLine(jankenAA[jank]);
+        }
+
+        public static Random rand = new Random();
+
+        public static int Janken()
+        {
+            int jank = rand.Next(3);
+            return jank;
+        }
+
+        public static void Judge(int player, int cpu)
+        {
+            if ((player == 0 && cpu == 1) || (player == 1 && cpu == 2) || (player == 3 && cpu == 0))
+            {
+                Console.WriteLine("プレイヤーが勝ち");
+            }
+            else if ((cpu == 0 && player == 1) || (cpu == 1 && player == 2) || (cpu == 3 && player == 0))
+            {
+                Console.WriteLine("CPUが勝ち");
+            }
+            else
+            {
+                Console.WriteLine("あいこ");
+            }
+            Console.ReadKey(true);
         }
     }
 }
